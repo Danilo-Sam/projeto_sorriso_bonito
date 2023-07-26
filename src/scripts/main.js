799b9e2fa8fa87b0e4ca5bfd4495525a6d37d980
Jânderson Machado <jandersonmachado090@gmail.com>
2023-07-26 18:42:09 -0300

    alteração no Gulp e atualização do projeto

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+const menuConteudo = document.getElementById('menu');
+
+function animar() {
+    if (menuConteudo.style.display === 'block') {
+        menuConteudo.style.display = 'none'; 
+    } else {
+        menuConteudo.style.display = 'block'; 
+    }
+}
+
+
