cca065f841e0a2851bb30fd44c2a97f8be295e7e
Aldo Santos <aldoacesso@gmail.com>
2023-08-01 08:51:17 -0300

    Criação da seção FAQ

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,4 +8,19 @@ function animar() {
     }
 }
 
+// Faq +++
+document.addEventListener('DOMContentLoaded', function() {
+    const questions = document.querySelectorAll('[data-faq-question]');
+    
+    
+    for(let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+})
 
+function abreOuFechaResposta(elemento) {
+    const classe ='faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
